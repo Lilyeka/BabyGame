86821959bb79b99f8aee6415273ade45e20a6ab0
Lilyeka <lzbox@mail.ru>
2016-01-07 15:42:10 +0500

    Update view1.js

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,10 +1,20 @@
 'use strict';
 
+//randomize = function(data)
+//{
+//var qwests = [];
+function getRandomInt(min, max) 
+{ 
+	return Math.floor(Math.random() * (max - min + 1)) + min; 
+} 
+	
+//}
 angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', function($scope, $http, $location) {
  $http.get('imgs.json').success(function(data){
-		$scope.questions = data;  
+
+		$scope.questions = randomize(data);  
  });   
  $scope.i = 0; 
  
