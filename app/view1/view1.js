6fb0926a1abec640f5dbed0a0869445b8664741b
Lilyeka <lzbox@mail.ru>
2016-01-08 02:16:06 +0500

    Update view1.js

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -40,23 +40,24 @@ angular.module('myApp.view1', ['ngRoute','ngAnimate'])
 
 })
 
-.animation('.view-slide-in', function(){
-return{
-	enter: function(element, done){
-		element.css({
-			opacity:0.5,
-			position:"relative",
-			top:"10px",
-			left:"20px"
-			
-		})
-    		.animate({
-    			top:0,
-			left:0,
-			opacity: 1},
-		1000,done);
-		
-	}
-  };
-});
+//.animation('.view-slide-in', function(){
+//return{
+//	enter: function(element, done){
+//		element.css({
+//			opacity:0.5,
+//			position:"relative",
+//			top:"10px",
+//			left:"20px"
+//			
+//		})
+  //  		.animate({
+    //			top:0,
+//			left:0,
+//			opacity: 1},
+//		1000,done);
+//		
+//	}
+//  };
+//})
+;
 
