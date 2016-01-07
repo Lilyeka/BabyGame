01c0014304f6114aaae3618ff9fad1df393995f7
Lilyeka <lzbox@mail.ru>
2016-01-07 16:16:36 +0500

    Update view1.js

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,21 +3,23 @@
 //randomize = function(data)
 //{
 //var qwests = [];
-function getRandomInt(min, max) 
-{ 
-	return Math.floor(Math.random() * (max - min + 1)) + min; 
-} 
+//function getRandomInt(min, max) 
+//{ 
+//	return Math.floor(Math.random() * (max - min + 1)) + min; 
+//} 
 	
 //}
 angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', function($scope, $http, $location) {
  $http.get('imgs.json').success(function(data){
-
-		$scope.questions = randomize(data);  
+	$scope.questions = data;  
  });   
- $scope.i = 0; 
- 
+ $scope.i = 0;
+ $scope.randomInt = 0;
+ $scope.setRandomInt = function(min,max){
+ 	$scope.randomInt = Math.floor(Math.random() * (max - min + 1)) + min;	
+ }
  $scope.setAnswer = function(answ,quests){
 		$scope.mainAnswer = answ;
 		if($scope.i + 1 <quests.length){
