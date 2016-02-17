0d49b8a1936ddabdd6f2b45c280f58ede7a531d7
Lilyeka <lzbox@mail.ru>
2016-02-17 19:57:07 +0500

    игра пазл - просто две картинки

--- app/puzzle/puzzle.js
+++ app/puzzle/puzzle.js
@@ -30,7 +30,7 @@ angular.module('myApp.puzzle', ['ngRoute'])
 
 	$scope.myStyle = {
 		//"background-color": "red",
-		"background-image": "img/12.jpg"
+		"background-image": url("img/12.jpg")
 		//"background-size": $scope.gridSize*100 + '%'
 	}
 
