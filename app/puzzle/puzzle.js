e601f2184a1ec2b07bfef048f867777bb66a2929
Lilyeka <lzbox@mail.ru>
2016-02-19 13:56:36 +0500

    игра пазл - просто две картинки

--- app/puzzle/puzzle.js
+++ app/puzzle/puzzle.js
@@ -45,7 +45,11 @@ angular.module('myApp.puzzle', ['ngRoute'])
 
 	$scope.label = "Please click";
 	$scope.doSomething = function(){
-		$scope.message = "Clicked!";
+		for (var i = 0; i < 9; i++) {
+			$scope.message = "Clicked!" + i;
+
+		}
+		//$scope.message = "Clicked!";
 	};
 
 
