6f8f6cf4f8af72dcdb9ededaa8ab8318d7c8133a
Lilyeka <lzbox@mail.ru>
2016-02-22 20:14:26 +0500

    Update puzzle.js

--- app/puzzle/puzzle.js
+++ app/puzzle/puzzle.js
@@ -60,7 +60,7 @@ angular.module('myApp.puzzle', ['ngRoute'])
 		$scope.cellSize = 400/gridSize;
 		for (var i = 0; i < 9; i++) {
 		//if ($scope.i<9){
-			$scope.veggies1.push({ "number": $scope.veggies1.length });
+			$scope.veggies1.push({ "number": '<button ng-click='alert("123")'>" + $scope.veggies1.length + "</button>' });
 			$scope.i = $scope.i + 1;
 		//}
 		}
