4061e8a5ca3c976b59b882afb6a7c31b56a92e75
Lilyeka <lzbox@mail.ru>
2016-02-24 19:34:02 +0500

    игра пазл - просто две картинки

--- app/puzzle/puzzle.js
+++ app/puzzle/puzzle.js
@@ -44,7 +44,8 @@ angular.module('myApp.puzzle', ['ngRoute'])
 	tmpl += '</td></table>';
 	return {
 		//template:parametr//"<button ng-click='doSomething()'>{{label}}</div>"
-		template:tmpl//"<button ng-click='doSomething()'>{{veg1.number}}</div>"
+		template:tmpl,
+		scope:true//"<button ng-click='doSomething()'>{{veg1.number}}</div>"
 	};
 })
 //.directive("otcDynamic", function($compile){
