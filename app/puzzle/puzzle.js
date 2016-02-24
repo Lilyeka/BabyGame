fa40360aecd00db1113b823b32b2d6a6ca3ad212
Lilyeka <lzbox@mail.ru>
2016-02-24 18:34:07 +0500

    игра пазл - просто две картинки

--- app/puzzle/puzzle.js
+++ app/puzzle/puzzle.js
@@ -23,6 +23,7 @@ return qwests;
 angular.module('myApp.puzzle', ['ngRoute'])
 .directive("otcDynamic", function(){
 	var tmpl = '<table>';
+	var img = $scope.puzzleimg;
 	// 9 заменить на gridSize*gridSize!
 	for (var i =0; i < 25; i++) {
 		// 3 заменить на gridSize!
@@ -33,7 +34,7 @@ angular.module('myApp.puzzle', ['ngRoute'])
 			tmpl += '<tr>';
 		}
 		tmpl += '<td>' +
-				'<li style="background-size:300%; background-image: url('+$scope.puzzleimg+'); width:100px; height:100px">'+ i +'</li>'+ '</td>';
+				'<li style="background-size:100%; background-image: url('+img+'); width:100px; height:100px">'+ i +'</li>'+ '</td>';
 			//'<div style="background-image: url(' + $scope.puzzleimg + '); background-size:300%; border-width:3px; border-color:blue; padding:4px">' + i + "</div></td>";
 	}
 
