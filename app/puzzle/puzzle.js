216c33d27bd7fab08439807968efb81cd0f8405f
Lilyeka <lzbox@mail.ru>
2016-03-26 01:02:53 +0600

    Update puzzle.js

--- app/puzzle/puzzle.js
+++ app/puzzle/puzzle.js
@@ -58,6 +58,23 @@ angular.module('myApp.puzzle', ['ngRoute'])
 
 
 .controller('PuzzleCtrl', function($scope, $http, $location,$compile) {
+	$scope.models = {
+        selected: null,
+        lists: {"A": [], "B": []}
+    };
+
+    // Generate initial model
+    for (var i = 1; i <= 3; ++i) {
+        $scope.models.lists.A.push({label: "Item A" + i});
+        $scope.models.lists.B.push({label: "Item B" + i});
+    }
+
+    // Model to JSON for demo purpose
+    $scope.$watch('models', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+    }, true);
+    
+    /*---------------------------------------------*/
 	$scope.puzzleimg = 'img/12.jpg';
 	$scope.veggies1 =[];
 	$scope.gridSize = 3;
