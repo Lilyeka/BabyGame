bd14d6150c9afd372c00ed1b9a6cd40bd4e04ae4
Lilyeka <lzbox@mail.ru>
2016-03-29 15:15:45 +0500

    Разбираемся с  drag & drop

--- app/app.js
+++ app/app.js
@@ -3,12 +3,12 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'dndLists',
   'myApp.welcome',
   'myApp.view1',
   'myApp.view2',
   'myApp.puzzle',
   'myApp.version',
-  'dndLists'
 ]).
 config(['$routeProvider', function($routeProvider) {
   
